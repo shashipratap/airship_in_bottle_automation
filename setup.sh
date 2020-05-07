749b7168f337dd693f5ec70d6cc87d6f4ef23bcb
root <root@ip-172-31-33-53.us-east-2.compute.internal>
2020-05-07 05:40:35 +0000

    new setup file

--- setup.sh
+++ setup.sh
@@ -1,44 +1,54 @@
 #!/bin/bash
 set -x
-echo "#####downloading kubelet,kuebadm and kubectl#####"
-wget https://storage.googleapis.com/kubernetes-release/release/v1.10.2/bin/linux/amd64/kubectl
-wget https://storage.googleapis.com/kubernetes-release/release/v1.10.2/bin/linux/amd64/kubelet
-wget https://storage.googleapis.com/kubernetes-release/release/v1.10.2/bin/linux/amd64/kubeadm
-chmod +x kubectl kubelet kubeadm
-sudo mv kubectl kubelet kubeadm /usr/local/bin/
-
-
+setup_local_repo()
+{
 echo "###########Setting up local ubuntu repo with docker.io , jq and nmap package deb files , other packages can be added as needed##########"
 echo "         #####Installing apache2 and dpkg#####"
 apt update
+apt install dpkg-dev -y
 apt install apache2 -y
+apt install wcstools -y
 systemctl enable apache2
 systemctl start apache2
-apt install dpkg-dev -y
 
 echo "         #####Downloading docker.io jq nmap curl deb files#####"
-mkdir -p /var/www/html/debs/amd64
-cd /var/www/html/debs/amd64
-PACKAGES="docker.io jq nmap curl"
+PKGDIR=${PKGDIR:-"/var/debs/amd64"}
+KUBE_BIN_DIR=${KUBE_BIN_DIR:-/var/www/html/kubedir}
+PACKAGES=${PACKAGES:-"docker.io jq nmap curl nfs-common ceph-common"}
+REPOS=${REPOS:-"quay.io docker.io gcr.io"}
+PKGDIRNAME=`filename $(realpath $PKGDIR)`
+VERSION_FILE=/root/deploy/airship-in-a-bottle/deployment_files/global/v1.0demo/software/config/versions.yaml
 
+
+if [ ! -d $PKGDIR ]
+then
+ mkdir -p $PKGDIR
+fi
+
+cd ${PKGDIR}
 sudo apt-get download $(sudo apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances $PACKAGES | grep "^\w" | sort -u)
-cd /var/www/html/debs
+cd $PKGDIR/..
 
 echo "         #####Configuring Apache for apt#####"
-dpkg-scanpackages amd64 | gzip -9c > amd64/Packages.gz
-cp /etc/apt/sources.list /etc/apt/sources.list.bkp && echo "deb [trusted=yes] http://localhost/debs/ amd64/" > /etc/apt/sources.list
+dpkg-scanpackages $PKGDIRNAME | gzip -9c > $PKGDIRNAME/Packages.gz
+cp /etc/apt/sources.list /etc/apt/sources.list.bkp && echo "deb [trusted=yes] file:////$PKGDIR/.. $PKGDIRNAME/" > /etc/apt/sources.list
 apt update
 apt install docker.io -y
-### optional to test working of script 
-apt install jq -y
-apt install curl -y
-apt install nmap -y
+### optional to test working of script
+for pkg in $PACKAGES
+do
+ apt install $pkg -y
+done
 
+}
 
+setup_local_registry()
+{
 ############Setting local registry
 echo "#####configuring local registry on port 5000 and putting pegleg and promenade image in it#####"
 
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
+echo "#########Pulling pegleg and promenade images to local repo###############"
 docker pull quay.io/airshipit/pegleg:ac6297eae6c51ab2f13a96978abaaa10cb46e3d6
 docker tag quay.io/airshipit/pegleg:ac6297eae6c51ab2f13a96978abaaa10cb46e3d6 localhost:5000/pegleg
 docker push localhost:5000/pegleg
@@ -49,13 +59,65 @@ docker tag quay.io/airshipit/promenade:master localhost:5000/promenade
 docker push localhost:5000/promenade
 docker rmi quay.io/airshipit/promenade:master localhost:5000/promenade
 
+echo "######making changes to deploy_airship.sh for local repo#########3"
+sed -i 's/PEGLEG_IMAGE=${PEGLEG_IMAGE:-"quay.io\/airshipit\/pegleg:ac6297eae6c51ab2f13a96978abaaa10cb46e3d6"}/PEGLEG_IMAGE=${PEGLEG_IMAGE:-"localhost:5000\/pegleg"}/' /root/deploy/airship-in-a-bottle/manifests/common/deploy-airship.sh
+sed -i 's/PROMENADE_IMAGE=${PROMENADE_IMAGE:-"quay.io\/airshipit\/promenade:master"}/PROMENADE_IMAGE=${PROMENADE_IMAGE:-"localhost:5000\/promenade"}/' /root/deploy/airship-in-a-bottle/manifests/common/deploy-airship.sh
 
-echo "#############Cloning the airship repo and making changes in deploy-airship.sh to fetch images from local repository##########"
+
+echo "##############downloading public repo images in /root/deploy/airship-in-a-bottle/deployment_files/global/v1.0demo/software/config/versions.yaml and making changes to refer to local repo###############"
+
+
+
+for repo in $REPOS;
+do
+        grep "$repo/" ${VERSION_FILE} | grep -v "^ *#" > /tmp/versions.$repo;
+        while read line;
+        do
+                echo "######yamlline: $line#######"
+                image=""
+                localrepo="localhost:5000"
+                image=$(echo $line | awk -F$repo -v VAR=$repo   '{print VAR$2}');
+                localimage=$(echo $line | awk -F$repo -v VAR=$localrepo  '{print VAR$2}');
+                echo "#############image: $image#########"
+                echo "###########localimage: $localimage##########"
+                docker pull $image
+                docker tag $image $localimage
+                docker push $localimage
+                docker rmi $image $localimage
+                echo $localimage; done < /tmp/versions.$repo;
+                echo "########changing ${VERSION_FILE} to refer to locarepo######"
+                sed -i "s@${repo}/@localhost:5000/@g" ${VERSION_FILE}
+
+done
+
+}
+
+download_kubernetes_packages()
+{
+
+echo "###########downloading kubernetes binaries locally#################"
+if [ ! -d ${KUBE_BIN_DIR} ]
+then
+ mkdir ${KUBE_BIN_DIR}
+fi
+wget https://dl.k8s.io/v1.10.2/kubernetes-node-linux-amd64.tar.gz ${KUBE_BIN_DIR}/kubernetes-node-linux-amd64.tar.gz
+echo "###########making local changes in $VERSION_FILE to point to local kubelet#################"
+sed -i "s@kubelet: https*.*@kubelet https://localhost/kubedir/kubernetes-node-linux-amd64.tar.gz@g" ${VERSION_FILE}
+}
+
+
+
+
+
 mkdir -p /root/deploy && cd "$_"
 git clone https://opendev.org/airship/airship-in-a-bottle
 cd /root/deploy/airship-in-a-bottle
 git checkout HEAD^1
-sed -i 's/PEGLEG_IMAGE=${PEGLEG_IMAGE:-"quay.io\/airshipit\/pegleg:ac6297eae6c51ab2f13a96978abaaa10cb46e3d6"}/PEGLEG_IMAGE=${PEGLEG_IMAGE:-"localhost:5000\/pegleg"}/' /root/deploy/airship-in-a-bottle/manifests/common/deploy-airship.sh 
-sed -i 's/PROMENADE_IMAGE=${PROMENADE_IMAGE:-"quay.io\/airshipit\/promenade:master"}/PROMENADE_IMAGE=${PROMENADE_IMAGE:-"localhost:5000\/promenade"}/' /root/deploy/airship-in-a-bottle/manifests/common/deploy-airship.sh
+
+setup_local_repo || error "setting up local ubuntu repo"
+setup_local_registry || error "setting local docker registry"
+download_kubernetes_packages || error "downloading kubernetes binaries"
+
 cd /root/deploy/airship-in-a-bottle/manifests/dev_single_node
-./airship-in-a-bottle.sh
+#./airship-in-a-bottle.sh
+
